091530b0dc05ff023b861c01c1d75de455abd504
lilithtransform <staticanne@skiff.com>
2023-01-16 19:01:13 -0800

    init docs

---
+++ source/conf.py
@@ -0,0 +1,31 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+import sys, os
+
+sys.path.insert(0, os.path.abspath('../..'))
+
+project = "cayde"
+copyright = "2023, Ethan Gallucci <phasewalk1>"
+author = "Ethan Gallucci <phasewalk1>"
+release = "0.1.0-alpha"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ["sphinx.ext.autodoc"]
+
+templates_path = ["_templates"]
+exclude_patterns = []
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "haiku"
+html_static_path = ["_static"]
---
+++ source/index.rst
@@ -0,0 +1,45 @@
+.. cayde documentation master file, created by
+   sphinx-quickstart on Mon Jan 16 18:36:14 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to cayde's documentation!
+=================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+
+.. automodule:: models
+   :members:
+
+.. autoclass:: models.ags.BinaryClassifierCNN
+   :members:
+   :private-members:
+.. autoclass:: models.ags.CRNNExtractorModel
+   :members:
+   :private-members:
+.. autoclass:: models.gcmfcc.GenreClassifierMFCC
+   :members:
+   :private-members:
+
+.. automodule:: util
+   :members:
+.. autoclass:: util.fetcher.Fetcher
+   :members:
+   :private-members:
+.. autoclass:: util.view.Viewer
+   :members:
+   :private-members:
+
+.. automodule:: wrangling
+   :members:
